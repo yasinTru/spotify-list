1f183de6a0390d273046942d9838333ea800992f
yasin dogru <yasindogru61@gmail.com>
2021-04-28 22:49:02 +0300

    init

---
+++ src/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: "0df32d524a654be8b15a11ab36fdf484",
+        ClientSecret: '6c2c54be3d17424b92bcf63eb21bc06b'
+    }
+}
+
+export { Credentials };
